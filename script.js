8648ca871e71684a8fdeb2f84a9e10c4d5efa321
Dani JM <xdanielmtz@gmail.com>
2021-03-01 18:05:25 -0500

    the model open with the button press

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ var currentId = 0;
 function addPersonToList(event) {
   event.preventDefault();
   var name = nameEl.value;
+  // Create the element
   var li = document.createElement("li");
+  // Add content 
   li.id = people.length;
   li.innerHTML = name + " <button>edit</button>";
+  // Append to existing element
   people.push({ name: name });
   peopleListEl.append(li);
 }
@@ -24,9 +27,20 @@ function close() {
   modalEl.style.display = "none";
 }
 
+function open() {
+  modalEl.style.display = "block";
+}
+
 function handleClick() {
   // Use event delegation to handle when the user clicks "edit"
 }
 
 closeEl.addEventListener("click", close);
 addBtn.addEventListener("click", addPersonToList);
+
+peopleListEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  if(event.target.matches("button")){
+    open();
+  }
+})
